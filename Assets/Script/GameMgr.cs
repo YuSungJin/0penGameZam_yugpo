fc1d0d0d9a6b7b252fdd1d7b36fe99937045015e
tigerlcw <tigerlcw>
2017-11-25 18:08:08 +0900

    앙기모띠주는나무의 나무 개발중

---
+++ Assets/Script/GameMgr.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMgr : MonoBehaviour {
+    public static GameMgr instance = null; //싱글턴
+    public GameObject[] TreeObj; //나무 오브젝트
+    public GameObject TreeArea; //나무 생성 위치
+
+    void Awake()
+    {
+        if(instance==null)
+        {
+            instance = this;
+        }
+    }
+
+
+
+	public void CreateTree() //나무 생성
+    {
+        int rand = Random.Range(0, 7);
+        Instantiate(TreeObj[rand]);
+        TreeObj[rand].transform.SetParent(TreeArea.transform);
+        TreeObj[rand].SetActive(true);
+    }
+
+    
+}
---
+++ Assets/Script/TreeCtrl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreeCtrl : MonoBehaviour {
+
+    [Header("Tree Info")]
+    public Image Treeimg; //나무 이미지
+    public string Treename; //나무 이름
+    public int Treelv; //나무 레벨
+    public int Treehp; //나무 체력
+
+
+	void Start () {
+        Treeimg = GetComponentInChildren<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Treehp<=0)
+        {
+            Die();
+        }
+
+	}
+
+
+    void Die()
+    {
+        Debug.Log("TreeDie");
+    }
+
+    public void SetTreeUI() //트리 UI 설정
+    {
+
+    }
+
+    public void HitTree(int damage) //나무 클릭(데미지)
+    {
+        Treehp -= damage;
+        HpBar.fillAmount -= (float)damage / Treehp;
+
+    }
+}
